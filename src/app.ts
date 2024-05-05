f0f7406d363b4c232ec01966b64c2aa41d706222
bayek0fsiwa <imvikas@tutanota.com>
2024-05-05 16:59:11 +0530

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express, { NextFunction, Request, Response } from "express";
+// import cors from "cors";
 import globalErrrorHandler from "./middlewares/globalErrorHandler";
 import railRouter from "./rail/railRouter";
 
 const app = express();
+// app.use(cors<Request>());
+
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
     res.status(200).json({ message: "Hello!" });
